45b5712f2bb2e8435d4f656be29505a12a0a7199
francobertin6 <61636447+francobertin6@users.noreply.github.com>
2020-06-28 22:36:57 -0300

    progress

--- scripts/delilah-resto.js
+++ scripts/delilah-resto.js
@@ -115,6 +115,57 @@ async function getinfo_products(event){
         }
         console.log(pedidos_data.id);
     };
+    let checked = pedidos_data.id.some(element => element >= 1);
+    console.log(checked);
+    if(checked === true){
+        carrito_noactivo.style.display = "none";
+        carrito_activa.style.display = "block";
+    }else{
+        carrito_noactivo.style.display = "block";
+        carrito_activa.style.display = "none";
+    }
+}
+
+let carrito_noactivo = document.getElementById("carrito-no-activo");
+let carrito_activa = document.getElementById("carrito-activo");
+let icons = document.getElementById("icons");
+let recomendados = document.getElementById("recomended");
+let nuestros_platos = document.getElementById("nuestros-platos");
+let pedidos = document.getElementById("pedidos");
+
+carrito_activa.addEventListener("click", async function(){
+    icons.style.display = "none";
+    recomendados.style.display = "none";
+    nuestros_platos.style.display = "none";
+    pedidos.style.display = "flex";
+    let detalle_pedido = document.createElement("section");
+    detalle_pedido.id = "first-half"; 
+    pedidos.appendChild(detalle_pedido);
+    let detalle = document.createElement("h1");
+    detalle_pedido.appendChild(detalle);
+    detalle.innerHTML = "detalle";
+    for (let index = 0; index < pedidos_data.id.length; index++) {
+        const element = pedidos_data.id[index];
+        let url = 'http://127.0.0.1:3000/productos/get/'+ element;
+        const resp = await fetch(url);
+        const data = await resp.json();
+        createpedidosxproductos_section(data, detalle_pedido);
+    }
+})
+
+function createpedidosxproductos_section(data, detalle_pedido){
+    let div_producto = document.createElement("div");
+    div_producto.className = "div_producto";
+    let foodname = document.createElement("h3");
+    let price = document.createElement("p");
+    let image = document.createElement("img");
+    detalle_pedido.appendChild(div_producto);
+    div_producto.appendChild(foodname);
+    div_producto.appendChild(price);
+    div_producto.appendChild(image);
+    foodname.innerHTML = data.datos[0].foodname;
+    price.innerHTML = data.datos[0].price;
+    image.src = data.datos[0].url;
 }
 
 
